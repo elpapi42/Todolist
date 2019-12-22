970604501c9cea4cc1a37722dfd3a6d15f8f0edc
ElPapi42 <whitman-2@hotmail.com>
2019-12-22 09:59:31 -0400

    Added dependecies to setup.py
    Now Github Actions run setup.py install for setup test enviroment

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='todolist',
-    packages=['todolist'],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=[
         'flask',
+        'flask-restful',
+        'flask-sqlalchemy',
+        'validator_collection'
     ],
 )
