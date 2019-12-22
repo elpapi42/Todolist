c37e717c289a61e85959d6d33afe6925a7d44892
ElPapi42 <whitman-2@hotmail.com>
2019-12-22 10:07:39 -0400

    Set min version for flask dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask',
+        'flask>=0.10.1',
         'flask-restful',
         'flask-sqlalchemy',
         'validator_collection'
