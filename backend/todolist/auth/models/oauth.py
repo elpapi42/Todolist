178624af9523f8cfca7a381990309a11347a192e
ElPapi42 <whitman-2@hotmail.com>
2019-12-24 01:15:41 -0400

    Implement Flask-Dance for OAuth with github

--- backend/todolist/auth/models/oauth.py
+++ backend/todolist/auth/models/oauth.py
@@ -11,9 +11,11 @@ class OAuth(OAuthConsumerMixin, db.Model):
 
     id = db.Column(UUID(as_uuid=True), primary_key=True, unique=True, nullable=False)
     user_id = db.Column(UUID(as_uuid=True), db.ForeignKey(User.id))
-    user = db.relationship(User)
+    user = db.relationship(User, uselist=False)
 
-    def __init__(self, user_id, user):
+    def __init__(self, provider, token, user_id, user):
+        self.provider = provider
+        self.token = token
         self.user_id = user_id
         self.user = user
         self.id = uuid.uuid4()
