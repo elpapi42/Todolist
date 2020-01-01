253d62e4f9b7433469fba9b8c04aae1ab6c1d646
ElPapi42 <whitman-2@hotmail.com>
2020-01-01 17:12:00 -0400

    Auth Process now return the API Access Token
    The user id is attached for allow queries about the user

--- backend/todolist/auth/routes.py
+++ backend/todolist/auth/routes.py
@@ -1,8 +1,9 @@
 from flask_restful import Api
 
 from . import auth_bp
-from .controllers import Logout
+from .controllers import Logout, IssueToken
 
 auth = Api(auth_bp)
 
 auth.add_resource(Logout, "/logout/")
+auth.add_resource(IssueToken, "/token/")
