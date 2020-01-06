0c77242d1079c971fb669ddaee1490d0ec05f302
ElPapi42 <whitman-2@hotmail.com>
2020-01-06 19:18:46 -0400

    Added temporal command for generate admin tokens
    This is supposed to be removed or replaced by a fully admin users implementation
    Actually it does not associate the token with any user

--- backend/todolist/__init__.py
+++ backend/todolist/__init__.py
@@ -12,6 +12,7 @@ login_manager = LoginManager()
 from todolist.api import api_bp
 from todolist.web import web_bp
 from todolist.auth import auth_bp, github_bp
+from todolist.cli import cli_bp
 
 def create_app(testing=False):
     """ 
@@ -45,6 +46,7 @@ def create_app(testing=False):
         app.register_blueprint(api_bp, url_prefix="/api")
         app.register_blueprint(auth_bp, url_prefix="/auth")
         app.register_blueprint(github_bp, url_prefix="/auth")
+        app.register_blueprint(cli_bp, cli_group=None)
         
         # Drop all the tables from test database if in test mode
         if(app.testing):
