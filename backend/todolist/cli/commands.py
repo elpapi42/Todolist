ad74559bce6d32b6e6e4edd5627121b97bc7396b
ElPapi42 <whitman-2@hotmail.com>
2020-01-11 17:51:22 -0400

    Added comand for give admin to user by email

--- backend/todolist/cli/commands.py
+++ backend/todolist/cli/commands.py
@@ -4,9 +4,29 @@ import os
 import jwt
 
 import click
+from validator_collection import is_email
 
 from . import cli_bp
+from .. import db
+from ..api.models import User
 
-@cli_bp.cli.command("blank-command")
-def generate_admin_token():
-    print("This does nothing")
+@cli_bp.cli.command("admin")
+@click.argument("email")
+def make_user_admin(email):
+
+    if(not is_email(email)):
+        print("Invalid Email")
+        return False
+
+    user = User.query.filter(User.email == email).first()
+    if(not user):
+        print("Email Not Registered")
+        return False
+
+    user.is_admin = True
+    db.session.commit()
+    print("Now {} has Admin Access".format(email))
+    
+    return True
+    
+    
